c431a5bfacde7ea3732a9cfde34c9f8f7506d6ee
AJIEKCEY <sergheialexei@gmail.com>
2024-10-12 21:06:22 +0300

    Fix after code review Add validation in profile component

--- src/components/Molecules/FormControl/formControl.tpl.ts
+++ src/components/Molecules/FormControl/formControl.tpl.ts
@@ -2,8 +2,10 @@ export default `
 <div class="form-control">
   {{#switch tag}}
     {{#case 'input'}}
-      <label for="{{id}}">{{label}}</label>
-      {{{FormElement}}}
+      <div>
+        <label for="{{id}}">{{label}}</label>
+        {{{FormElement}}}
+      </div>
       <span class="validationMessage">{{validationMessage}}</span>
     {{/case}}
     {{#case 'button'}}
